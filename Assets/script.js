f8f5a55ebb1dc594a0e145f96db4f301477a32ec
Zach Oda <zjodachow@gmail.com>
2022-07-06 14:21:18 -0400

    save function added

--- Assets/script.js
+++ Assets/script.js
@@ -1,30 +1,30 @@
 //creating variable for moment.js
 var currentTime = moment().format("[Today is...] dddd, MMMM D YYYY, h:mm:ss a");
 $("#currentDay").text(currentTime);
-//variable for capturing task data 
-var taskDescription = $("description")
+
+//variable for capturing task data
+var taskDescription = $(".description").val();
 
 //variables for determining color of row
 var currentHour = moment().format("k");
-var taskHour = $("hour")
+var taskHour = $(".hour");
 // function for determining color or row
 var urgentTask = function () {
-if (currentHour == taskHour) {
-    $(description).addClass("present")
-} else if (currentHour < taskHour) {
-    $(description).addClass("future")
-} else (currentHour > taskHour) 
-    $(description).addClass("past")
+  if (currentHour == taskHour) {
+    $(description).addClass("present");
+  } else if (currentHour < taskHour) {
+    $(description).addClass("future");
+  } else currentHour > taskHour;
+  $(description).addClass("past");
 };
 //save function
-var saveTasks = function() {
-    $(".saveBtn").on("click", "description", function()); {
-    if(description === !null) {
-        localStorage.setItem("hour", taskHour)
-        localStorage.setItem("description", taskDescription)
-}
-}
-};
+$(".saveBtn").on("click", function () {
+  var taskDescription = $(this).siblings(".description").val();
+  console.log(taskDescription);
+  if (taskDescription === !null) {
+    localStorage.setItem("hour", taskHour);
+    localStorage.setItem("description", taskDescription);
+  }
+});
 
 //load function
-
