ef63173cd5d9c74688f6d0b44722f912d7add8e2
Zach Oda <zjodachow@gmail.com>
2022-07-07 18:58:39 -0400

    updated save function

--- Assets/script.js
+++ Assets/script.js
@@ -1,33 +1,48 @@
 //creating variable for moment.js
 var currentTime = moment().format("[Today is...] dddd, MMMM D YYYY, h:mm:ss a");
 $("#currentDay").text(currentTime);
-
+var taskHour = $(this).attr("id");
 //variable for capturing task data
 var taskDescription = $(".description").val();
 
 //variables for determining color of row
 var currentHour = moment().format("k");
-var taskHour = $(".hour");
-// function for determining color or row
-var urgentTask = function () {
-  if (currentHour == taskHour) {
-    $(description).addClass("present");
-  } else if (currentHour < taskHour) {
-    $(description).addClass("future");
-  } else currentHour > taskHour;
-  $(description).addClass("past");
-};
+
+$(".time-block").each(function(){
+  var taskHour = $(this).attr("id");
+  for (var i=0; i < currentHour.length; i++);
+    if (currentHour === taskHour) {
+      $(".description").addClass("present");
+    } else if (currentHour < taskHour) {
+      $(".description").addClass("future");
+    } else (currentHour > taskHour);
+    $(".description").addClass("past");
+    console.log(taskHour);
+});
+
+
 //save function
 $(".saveBtn").on("click", function () {
   var taskDescription = $(this).siblings(".description").val();
   console.log(taskDescription);
+  var taskHour = $(this).parent();
+  var hourID = taskHour[0].id; 
   if (taskDescription === !null) {
-    localStorage.setItem("hour", taskHour);
-    localStorage.setItem("description", taskDescription);
+    localStorage.setItem("hour", JSON.stringify(hourID));
+    localStorage.setItem("description", JSON.stringify(taskDescription));
+    console.log("work");
+  }else {
+    console.log("error")
   }
-  JSON.stringify(taskHour);
   console.log(taskHour);
+  console.log(hourID);
 });
 
 //load function
+var reloadPlanner = function() {
+  var reloadHour = localStorage.getItem("hour");
+  JSON.parse(reloadHour);
+  var reloadTask = localStorage.getItem("description");
+  JSON.parse(reloadTask);
+}
 
