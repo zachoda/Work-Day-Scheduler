7bc15465f5cf3f2bf36e0a736a3484d56154f7a9
Zach Oda <zjodachow@gmail.com>
2022-07-07 21:45:33 -0400

    load function added

--- Assets/script.js
+++ Assets/script.js
@@ -2,8 +2,6 @@
 var currentTime = moment().format("[Today is...] dddd, MMMM D YYYY, h:mm:ss a");
 $("#currentDay").text(currentTime);
 var taskHour = $(this).attr("id");
-//variable for capturing task data
-var taskDescription = $(".description").val();
 
 //variables for determining color of row
 var currentHour = moment().format("k");
@@ -13,36 +11,35 @@ $(".time-block").each(function(){
   for (var i=0; i < currentHour.length; i++);
     if (currentHour === taskHour) {
       $(".description").addClass("present");
-    } else if (currentHour < taskHour) {
+    } else if (currentHour > taskHour) {
       $(".description").addClass("future");
-    } else (currentHour > taskHour);
+    } else (currentHour < taskHour);
     $(".description").addClass("past");
-    console.log(taskHour);
 });
 
 
 //save function
 $(".saveBtn").on("click", function () {
   var taskDescription = $(this).siblings(".description").val();
-  console.log(taskDescription);
   var taskHour = $(this).parent();
   var hourID = taskHour[0].id; 
-  if (taskDescription === !null) {
-    localStorage.setItem("hour", JSON.stringify(hourID));
-    localStorage.setItem("description", JSON.stringify(taskDescription));
-    console.log("work");
-  }else {
-    console.log("error")
+  if (taskDescription !== null) {
+  localStorage.setItem("hour" + hourID, JSON.stringify(hourID));
+    localStorage.setItem("description" + hourID, taskDescription);
   }
-  console.log(taskHour);
-  console.log(hourID);
 });
 
 //load function
 var reloadPlanner = function() {
-  var reloadHour = localStorage.getItem("hour");
-  JSON.parse(reloadHour);
-  var reloadTask = localStorage.getItem("description");
-  JSON.parse(reloadTask);
+  for (let i= 9; i<18; i++) {
+    var reloadHour = localStorage.getItem("hour" + i);
+    console.log(reloadHour);
+    var reloadTask = localStorage.getItem("description" + i);
+    document.getElementById("d-" + i).textContent = JSON.parse(reloadTask);
+  }
+  
+  
+  
 }
+reloadPlanner();
 
